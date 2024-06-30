e11922704f69311a7641e7ed9b256f14013414fa
npogo <npogoncuk@gmail.com>
2024-06-30 15:47:07 +0300

    Test TicTacToe field drawing

--- app/src/main/java/com/nazar/tictactoecustomview/MainActivity.kt
+++ app/src/main/java/com/nazar/tictactoecustomview/MainActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.nazar.tictactoecustomview.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.randomButton.setOnClickListener {
+            binding.ticTacToeView.ticTacToeField = TicTacToeField(Random.nextInt(3, 10), Random.nextInt(3, 10)).also { field ->
+                for (i in 0 until field.rows) {
+                    for (j in 0 until field.columns) {
+                        field[i, j] = when(Random.nextInt(3)) {
+                            0 -> Cell.PLAYER_X
+                            1 -> Cell.PLAYER_O
+                            else -> Cell.EMPTY
+                        }
+                    }
+                }
+            }
         }
     }
 }
